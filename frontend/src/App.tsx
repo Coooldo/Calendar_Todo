5c3f6e6ee8cfe8f2ee5b9e18132d4bf1f7abd5f6
Ryokuman <ryokuman@naver.com>
2022-09-06 21:38:35 +0900

    ADD: index.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Details, Login, MyPage, SignUp, Intro } from "./Pages";
+import { Header, Footer } from "./Components";
 
 function App() {
-    return <div>hello</div>;
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Intro />} />
+                <Route path="/Details" element={<Details />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/MyPage" element={<MyPage />} />
+                <Route path="/SignUp" element={<SignUp />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/Pages/index.tsx
@@ -0,0 +1,7 @@
+import Details from "./Details";
+import Login from "./Login";
+import MyPage from "./MyPage";
+import SignUp from "./SignUp";
+import Intro from "./Intro";
+
+export { Details, Login, MyPage, SignUp, Intro };
